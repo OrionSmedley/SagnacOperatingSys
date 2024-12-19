b898ae312e3d3b016ac9a09a4e554bc9ad1fcb7c
Sagnac 1.0 (fiber) <61481323+OrionSmedley@users.noreply.github.com>
2024-12-19 04:59:00 -0500

    Simplified CMD to few lines

--- MinimalSagnac/sagnacMultitool/Shortcut.cmd
+++ MinimalSagnac/sagnacMultitool/Shortcut.cmd
@@ -5,23 +5,15 @@ echo Your File is "%~1"
 call conda activate pipPymeasMod
 cd /d "%~dp0"
 
-:: Ask user what they want to do
-set /p choice=Enter your choice - [p]lot, [r]un, [a]ll: 
 
-
-:: Run the appropriate script
+set /p choice= [P]lot? (default no)
 if "%choice%"=="p" (
-    python plotter.py "%~1"  
+    start /b python plotter.py "%~1"  
 )
 
+set /p choice= [r]un? (default no)
 if "%choice%"=="r" (
     python experimenter.py "%~1"
-)
-
-if "%choice%"=="a" (
-    start /b python plotter.py "%~1"  
-    :: Runs plotter.py in the background
-    python experimenter.py "%~1" 
 )
 
 cmd /k
