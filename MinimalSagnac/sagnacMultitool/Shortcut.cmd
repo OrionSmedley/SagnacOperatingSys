a578d844b4787f7c27a044809e639de32cdf2fe5
Sagnac 1.0 (fiber) <61481323+OrionSmedley@users.noreply.github.com>
2024-12-19 04:43:09 -0500

    shortcut works

--- MinimalSagnac/sagnacMultitool/Shortcut.cmd
+++ MinimalSagnac/sagnacMultitool/Shortcut.cmd
@@ -1,29 +1,40 @@
-:: Initialize environment
-call conda activate pipPymeasMod
-cd /d "%~dp0"
-
-:: Ask user what they want to d
+:: Ask user what they want to do
+@echo off
 echo Your File is "%~1"
 set /p choice=Enter your choice - [v]iew, [p]lot, [r]un, [a]ll: 
 
 
-
-
 :: Run the appropriate script
+
+
+
+
 if "%choice%"=="v" (
-    code "%~1"
-) else if "%choice%"=="p" (
-     start /b python plotter.py "%~1"  
-     :: Runs plotter.py in the background
-) else if "%choice%"=="r" (
+    code "%~1"  
+)
+
+if "%choice%"=="p" (
+    call conda activate pipPymeasMod
+    cd /d "%~dp0"
+
+    python plotter.py "%~1"  
+)
+
+if "%choice%"=="r" (
+    call conda activate pipPymeasMod
+    cd /d "%~dp0"
+
     python experimenter.py "%~1"
-) else if "%choice%"=="a" (
+)
+
+if "%choice%"=="a" (
+    call conda activate pipPymeasMod
+    cd /d "%~dp0"
+
     start /b python plotter.py "%~1"  
     :: Runs plotter.py in the background
-    python experimenter.py "%~1"     
-    :: Runs experimenter.py after plotter.py starts
-) else (
-    echo Invalid choice. Exiting.
+    python experimenter.py "%~1" 
 )
 
 cmd /k
+
