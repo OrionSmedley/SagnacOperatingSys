aefae5663e1fe880bd68233349fb6be0d703c10e
Sagnac 1.0 (fiber) <61481323+OrionSmedley@users.noreply.github.com>
2024-12-19 04:02:16 -0500

    Update Shortcut.cmd

--- MinimalSagnac/sagnacMultitool/Shortcut.cmd
+++ MinimalSagnac/sagnacMultitool/Shortcut.cmd
@@ -1,11 +1,29 @@
-call conda activate pipPymeasMod  
-:: Activates the `pipPymeasMod` Conda environment
+:: Initialize environment
+call conda activate pipPymeasMod
+cd /d "%~dp0"
 
-cd /d "%~dp0"  
-:: Changes to the directory where this script is located (`%~dp0`), including changing drives if needed
+:: Ask user what they want to d
+echo Your File is "%~1"
+set /p choice=Enter your choice - [v]iew, [p]lot, [r]un, [a]ll: 
 
-python experimenter.py "%~1"  
-:: Runs `experimenter.py` with the first argument passed to this script (`%~1`)
 
-cmd /k  
-:: Keeps the Command Prompt open after execution to view output or errors
+
+
+:: Run the appropriate script
+if "%choice%"=="v" (
+    code "%~1"
+) else if "%choice%"=="p" (
+     start /b python plotter.py "%~1"  
+     :: Runs plotter.py in the background
+) else if "%choice%"=="r" (
+    python experimenter.py "%~1"
+) else if "%choice%"=="a" (
+    start /b python plotter.py "%~1"  
+    :: Runs plotter.py in the background
+    python experimenter.py "%~1"     
+    :: Runs experimenter.py after plotter.py starts
+) else (
+    echo Invalid choice. Exiting.
+)
+
+cmd /k
