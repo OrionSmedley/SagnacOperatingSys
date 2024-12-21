bbf51a33e5cb90330acd78c1db1ce83d7fe498e1
luogroup-usc <luogroup@usc.edu>
2024-12-21 00:11:42 -0800

    Minimal Error
    minimal error 2

--- MinimalSagnac/sagnacMultitool/play3.py
+++ MinimalSagnac/sagnacMultitool/play3.py
@@ -1,17 +1,2 @@
 from aparatus.sagnac3 import keith1, keith2, myHF2LI, mag     
-dem = myHF2LI.dem
-parameters = {}
-
-# this should have gone into the driver ... opps!
-def setXmag(b): mag.safeWaitCart(b,0,0)
-def setYmag(b): mag.safeWaitCart(0,b,0)
-def setZmag(b): mag.safeWaitCart(0,0,b)
-
-
-
-laserPow =  165 #mA
-lasertemp = 21.5 #C
-startI = -1e-3
-stopI = 1e-3
-
-setZmag(-0.01)
+mag.safeWaitCart(0,0,0)
