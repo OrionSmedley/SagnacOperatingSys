81cef1792393631b9c3f429c3fde4bd6b8dfe05a
Sagnac 1.0 (fiber) <61481323+OrionSmedley@users.noreply.github.com>
2025-02-03 19:47:22 -0500

    reference other custom instruments for sagnac 1

--- MinimalSagnac/sagnacMultitool/aparatus/sagnac1.py
+++ MinimalSagnac/sagnacMultitool/aparatus/sagnac1.py
@@ -38,7 +38,7 @@ logging.basicConfig(level=logging.WARNING,  # Log only WARNING or above by defau
 
 
 # MAGNET CONTROL
-from .drivers.custom_instruments import daedalusProjField
+from .drivers.custom_instruments1 import daedalusProjField
 from pymeasure.adapters import DAQmxAdapter
 
 calib_file = 'C:\\Users\\Ralph Group\\Documents\\Github\\SagnacOperatingSys\\sagnac_control\\calibrations\\sagnac'
