e6198323d53201e49cdb52ccf8f0f1992e6c7a9d
luogroup-usc <luogroup@usc.edu>
2025-05-02 18:41:27 -0700

    Adding Log to experimenter
    Added a line of code to experimenter to log scan completion to the csv.

--- MinimalSagnac/sagnacMultitool/experimenter.py
+++ MinimalSagnac/sagnacMultitool/experimenter.py
@@ -102,4 +102,4 @@ if __name__ == "__main__":
     print("Wrapping up and going home :) \n")
     # wrapUp(parameters, csv_file, namespace)
     exec("wrapUp()", namespace)
-
+    with open(csv_file, 'a') as f: f.write(f";#Scan finished at {pd.Timestamp.now().isoformat()}\n")
