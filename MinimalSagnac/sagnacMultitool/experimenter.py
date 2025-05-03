b2fb6844885b6968d7b52822fe6c0fe7628a2288
Orion Smedley H5 desktop <61481323+OrionSmedley@users.noreply.github.com>
2025-05-03 16:38:21 -0400

    update final print in experimenter

--- MinimalSagnac/sagnacMultitool/experimenter.py
+++ MinimalSagnac/sagnacMultitool/experimenter.py
@@ -99,7 +99,6 @@ if __name__ == "__main__":
     run_experiment(parameters, csv_file, namespace)
     
     print("_________________________________________________")
-    print("Wrapping up and going home :) \n")
-    # wrapUp(parameters, csv_file, namespace)
-    exec("wrapUp()", namespace)
+    print(f";#Scan finished at {pd.Timestamp.now().isoformat()}\n")
     with open(csv_file, 'a') as f: f.write(f";#Scan finished at {pd.Timestamp.now().isoformat()}\n")
+    exec("wrapUp()", namespace)
